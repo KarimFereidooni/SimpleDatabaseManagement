f054cdf174cd3f41e57d4bb1f36e0dd27fcc846e
karim fereidooni <kf.worknet@gmail.com>
2025-07-22 18:53:58 +0330

    Initial Commit

--- DatabaseManagement/Connection.cs
+++ DatabaseManagement/Connection.cs
@@ -11,7 +11,7 @@ namespace DatabaseManagement
         public string Server { get; set; }
         public string Authentication { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string Password { get; set; } = "";
         public string Database { get; set; }
     }
 }
